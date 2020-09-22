d69e90e9d519043bdb0a419a2910a881f9ecd6b8
eclectic_mind <52823445+eclectic-mind@users.noreply.github.com>
2020-09-23 00:03:39 +0300

    feat: recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -3,6 +3,6 @@ const CustomError = require("../extensions/custom-error");
 module.exports = class DepthCalculator {
   calculateDepth(array) {
     if (!array) return 0;
-    return Array.isArray(array) ? 1 + Math.max(...array.map(calculateDepth)) : 0;
+    return !Array.isArray(array) ? 0 : 1 + Math.max(...array.map(this.calculateDepth.bind(this)));
   }
 };
